cc86e6dd97273a44f9e4fb266053b731dd582ced
Cauã <silva.caua27@escola.pr.gov.br>
2025-05-09 11:19:12 -0300

    exemplodo5operacoesstring

---
+++ Capitulo2/Exemplo5operacoesstring.py
@@ -0,0 +1,10 @@
+# É proibido ultilizar operacoes matematicas com strings
+# ao menos em 2 casos
+
+first = 'Caua'
+second = ' Victor'
+nomeSobrenome = first + second # + aqu significa concatenacao
+print (nomeSobrenome)
+
+stringQualquer = 'teste ' * 3
+print(stringQualquer)
