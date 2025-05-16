5efa44a44d2386d18634a147b93df42931f51853
Cauã <silva.caua27@escola.pr.gov.br>
2025-05-16 09:57:02 -0300

    Exercicio3

---
+++ Capitulo2/Exercicio3.py
@@ -0,0 +1,17 @@
+print ('tempo do primeiro trecho')
+minutoPrimeiroTrecho = 8
+segundoPrimeiroTrecho = 15
+totalTempoPrimeiroTrecho = (8 * 60) + 15
+print("Tempo do primeiro trecho em segundos: ", totalTempoPrimeiroTrecho)
+
+print("Tempo do segundo trecho: 7min e 12seg")
+minutoSegundoTrecho = 7
+segundosSegundoTrecho = 12
+totalTempoSegundoTrecho = (7 * 60) + 12
+print("Tempo do segundo trecho em segundos: ", totalTempoSegundoTrecho)
+
+print("Tempo do terceiro trecho: 8 min e 15 s")
+minutoTerceiroTrecho = 8
+segundosTerceiroTrecho = 15
+totalTempoTerceiroTrecho = (8 * 60) + 15
+print("Tempo do terceiro trecho em segundos: ", totalTempoTerceiroTrecho)
