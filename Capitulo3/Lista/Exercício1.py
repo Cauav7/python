6cc38a0ad407294f110b3d0f61ca030c2e045929
Cauã <luis.mendes.almeida@escola.pr.gov.br>
2025-06-27 12:16:55 -0300

    27/06/25

--- Capitulo3/Lista/Exercício1.py
+++
@@ -1,8 +0,0 @@
-def justificar_direita(stringQualquer):
-    setentaEspacos = "          "
-    print(len(setentaEspacos))
-    print(setentaEspacos * 7 + stringQualquer)
-
-
-stringQualquer = "Cauã"
-justificar_direita(stringQualquer)
